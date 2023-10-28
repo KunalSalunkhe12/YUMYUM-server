2cb574ab9198e9b02b964b311f8fc5c7bc3123f9
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-10-28 16:26:01 +0530

    update user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: function (value) {
+                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+            },
+            message: "Invalid email address format",
+        },
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, "Password length must be at least 6 characters"],
+    },
     items: Object,
 
 });
