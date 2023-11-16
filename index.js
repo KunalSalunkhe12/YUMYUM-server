20a3dfa4c3a276b0de4347e3e7030118cb727cd9
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-11-16 15:21:49 +0530

    fix mogo url

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get('/search', (req, res) => {
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/yumyum', {
+        await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
