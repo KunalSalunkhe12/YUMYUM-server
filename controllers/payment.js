ce390d758817fe99f4dfaabbb02b9728f4dca612
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-11-17 15:29:30 +0530

    add try catch to payment

--- controllers/payment.js
+++ controllers/payment.js
@@ -3,31 +3,35 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const payment = async (req, res) => {
     const { cartData } = req.body;
 
-
-    const cartDataArr = Object.values(cartData);
-    const lineItems = cartDataArr.map((data) => ({
-        price_data: {
-            currency: "inr",
-            product_data: {
-                name: data.item.name,
-                images: [
-                    `https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${data.item.imageId}`
-                ],
+    try {
+        const cartDataArr = Object.values(cartData);
+        const lineItems = cartDataArr.map((data) => ({
+            price_data: {
+                currency: "inr",
+                product_data: {
+                    name: data.item.name,
+                    images: [
+                        `https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${data.item.imageId}`
+                    ],
+                },
+                unit_amount: data.item.price || data.item.defaultPrice,
             },
-            unit_amount: data.item.price || data.item.defaultPrice,
-        },
-        quantity: data.quantity,
-    }));
+            quantity: data.quantity,
+        }));
 
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: lineItems,
-        mode: "payment",
-        success_url: `${process.env.CLIENT_URL}/success`,
-        cancel_url: `${process.env.CLIENT_URL}/cancel`,
-    });
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: lineItems,
+            mode: "payment",
+            success_url: `${process.env.CLIENT_URL}/success`,
+            cancel_url: `${process.env.CLIENT_URL}/cancel`,
+        });
 
-    res.json({ url: session.url });
+        res.json({ url: session.url });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
 };
 
 module.exports = { payment };
