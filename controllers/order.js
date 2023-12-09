15e1f71f3f9a0d39c787f697a2edae072daea758
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-12-09 16:41:53 +0530

    sort order wrt createdAt

--- controllers/order.js
+++ controllers/order.js
@@ -3,7 +3,7 @@ const Order = require('../models/order');
 const getOrders = async (req, res) => {
     const { id: userId } = req.user;
     try {
-        const orders = await Order.find({ userId });
+        const orders = await Order.find({ userId }).sort({ created: -1 });
         res.status(200).json({ orders });
     } catch (error) {
         res.status(500).json({ message: error.message });
